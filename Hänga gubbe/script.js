c31cbad15aa8672fb664b3ac7e9aa45120beeaad
02Patrick02 <paku02001@utb.vaxjo.se>
2021-11-30 09:25:30 +0100

    klar

--- Hänga gubbe/script.js
+++ Hänga gubbe/script.js
@@ -5,6 +5,9 @@ let letterBoxes; // Array med referens till de span-taggar som utgör rutor för
 let hangmanImg; // Referens till img-elementet med bilden för galgen och gubben.
 let hangmanImgNr; // Nummer för aktuell bild (0-6), för den bildfil som som visas (så man sedan kan veta vilket som blir nästa bild).
 let msgElem; // Referens till div-elementet för medelandet.
+let startGameBtn; // Referens till startknappen
+let letterButtons; // Array med referenser till bokstavsknapparna 
+let startTime;
 // Globala variabler
 
 
@@ -12,13 +15,17 @@ let msgElem; // Referens till div-elementet för medelandet.
 // Initiering av globala variabler samt koppling av funktioner till knapparna.
 function init() {
 	let i; // Loopvariabel
-	let startGameBtn; // Referens till startknappen
-	let letterButtons; // Array med referenser till bokstavsknapparna 
 
-	startGameBtn = document.getElementById("startGameBtn").addEventListener("click", startGame);
+	startGameBtn = document.getElementById("startGameBtn");
+	startGameBtn.addEventListener("click", startGame);
+	
 
 	letterButtons = document.getElementById("letterButtons").getElementsByTagName("button");
-	for(i = 0; i < letterButtons.length; i++) letterButtons[i].onclick = guessLetter;
+	for(i = 0; i < letterButtons.length; i++){
+		letterButtons[i].addEventListener("click", guessLetter);
+
+	}
+
 
 	hangmanImg = document.getElementById("hangman");
 	msgElem = document.getElementById("message");
@@ -26,36 +33,117 @@ function init() {
 	wordList = ["BLOMMA","LASTBIL","SOPTUNNA","KÖKSBORD","RADIOAPPARAT","VINTER","SOMMAR","DATORMUS","LEJON","ELEFANTÖRA","JULTOMTE",
 				"SKOGSHYDDA","BILNUMMER","BLYERTSPENNA","SUDDGUMMI","KLÄDSKÅP","VEDSPIS","LJUSSTAKE","SKRIVBORD","ELDGAFFEL","STEKPANNA",
 				"KASTRULL","KAFFEBRYGGARE","TALLRIK","SOFFBORD","TRASMATTA","FLYGPLAN","FLYGPLATS","TANGENTBORD"];
-	
+
+	changeButtonActivation(true);
 } // End init
 window.onload = init; // Se till att init aktiveras då sidan är inladdad
 
 function startGame(){
+	let now;
 	randomWord();
 	showLetterBoxes();
-	hangmanImg = "pics/h0.png"
+
+	hangmanImg.src = "pics/h0.png"
 	hangmanImgNr = 0;
+
+	changeButtonActivation(false);
+
+	msgElem.innerHTML = "";
+
+	now = new Date();
+	startTime = now.getTime();
 } 
 
 function randomWord(){
-	let wordIndex;
-	for(wordIndex = 0; wordIndex < wordlist.length; wordIndex--){
-		wordIndex = Math.floor(Math.random())
-		return selectedWord[wordIndex];
+	let oldWord;
+	let wordIndex = Math.floor(Math.random() * wordList.length) + 1;
+	
+	while(oldWord == selectedWord){
+		selectedWord = wordList[wordIndex];
+		selectedWord[oldWord];
 	}
+	
 }
 
 function showLetterBoxes(){
-	let newCode;
+	let newCode = "";
 	let i;
+	for(i = 0; i < selectedWord.length; i++){
+		newCode += "<span>&nbsp;</span>";
+	}
+	document.getElementById("letterBoxes").innerHTML = newCode;
+	letterBoxes = document.getElementById("letterBoxes").getElementsByTagName("span");
 
-	newCode = "";
 }
 
 function guessLetter(){
+	let letter; //bokstaven för knapen
+	let i; // loopvariabel
+	let letterFound = false; //true eller false när man gissat
+	let correctLettersCount = 0; //räknare
+	
+	letter = this.value;
+	this.disabled = true;
 
+	for(i = 0; i < selectedWord.length; i++){
+		if(letter == selectedWord.charAt(i)){
+			letterBoxes[i].innerHTML = letter;
+			letterFound = true;
+		}
+		if(letterBoxes[i].innerHTML != "&nbsp;"){
+			correctLettersCount++;
+		}
+	}
+	
+	if(letterFound == false){
+		hangmanImgNr++;
+		hangmanImg.src = "pics/h" + hangmanImgNr + ".png";
+
+		if(hangmanImgNr == 6){
+			endGame(true);
+		}
+	}
+	else if(correctLettersCount === selectedWord.length){
+		endGame(false);
+	}
 }
 
 function endGame(manHanged){
-	
+	let now;
+	let runTime;
+
+	now = new Date();
+	runTime = (now.getTime() - startTime) / 1000;
+
+	if(manHanged == true){
+		msgElem.innerHTML = "Man got hanged " + selectedWord + " was the correct word in\n";
+		msgElem.innerHTML += runTime.toFixed(1) + " s";
+	}
+	else{
+		msgElem.innerHTML = "congratulations you won!!!! in\n";
+		msgElem.innerHTML += runTime.toFixed(1) + " s";
+	}
+
+
+
+	changeButtonActivation(true);
+
+}
+
+function changeButtonActivation(status){
+	console.log(status);
+	if(status == true){
+		startGameBtn.disabled = false;
+		for(i = 0; i < letterButtons.length; i++){
+			letterButtons[i].disabled = true;
+		}
+			
+	}
+	else if(status == false){
+		startGameBtn.disabled = true;
+		for(i = 0; i < letterButtons.length; i++){
+			letterButtons[i].disabled = false;
+		}
+			
+	}
 }
